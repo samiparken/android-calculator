9c20b89f850561a8799e4e9fc8654ff44039ae74
Han-Saem Park <samiparken@gmail.com>
2021-06-14 19:34:57 +0200

    use KotlinX extensions

--- app/src/main/java/com/samiparken/android_calculator/MainActivity.kt
+++ app/src/main/java/com/samiparken/android_calculator/MainActivity.kt
@@ -3,6 +3,7 @@ package com.samiparken.android_calculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
