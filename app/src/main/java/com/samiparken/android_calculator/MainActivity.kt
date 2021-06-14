9aac1a298163d40e9323d0cdbf8aa4d4b5bad30a
Han-Saem Park <samiparken@gmail.com>
2021-06-14 19:33:30 +0200

    add onDigit

--- app/src/main/java/com/samiparken/android_calculator/MainActivity.kt
+++ app/src/main/java/com/samiparken/android_calculator/MainActivity.kt
@@ -2,10 +2,15 @@ package com.samiparken.android_calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onDigit(view: View) {
+        Toast.makeText(this, "Button works", Toast.LENGTH_SHORT).show()
     }
 }
