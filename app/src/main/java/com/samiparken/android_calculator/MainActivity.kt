fbc15676bcc91da51344e8b8cf21f8fd181c782e
Han-Saem Park <samiparken@gmail.com>
2021-06-21 15:35:27 +0200

    change app_name

--- app/src/main/java/com/samiparken/android_calculator/MainActivity.kt
+++ app/src/main/java/com/samiparken/android_calculator/MainActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
