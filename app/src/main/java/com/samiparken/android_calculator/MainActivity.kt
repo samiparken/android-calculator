33e3c474be5c4ef923cdca70a718982873c83aad
Han-Saem Park <samiparken@gmail.com>
2021-06-21 12:28:22 +0200

    add fun onDecimalPoint

--- app/src/main/java/com/samiparken/android_calculator/MainActivity.kt
+++ app/src/main/java/com/samiparken/android_calculator/MainActivity.kt
@@ -8,13 +8,37 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var isInteger = false
+    var isEmpty = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
     fun onDigit(view: View) {
-        Toast.makeText(this, "Button works", Toast.LENGTH_SHORT).show()
-        tvInput.append( (view as Button).text )
+        Toast.makeText(this, "Digit", Toast.LENGTH_SHORT).show()
+        tvInput.append((view as Button).text)
+        if (isEmpty) {
+            isInteger = true
+            isEmpty = false
+        }
+    }
+
+    fun onClear(view: View) {
+        Toast.makeText(this, "Clear!", Toast.LENGTH_SHORT).show()
+        tvInput.text = ""
+        isInteger = false
+        isEmpty = true
+    }
+
+    fun onDecimalPoint(view: View) {
+        if (isEmpty) {
+            tvInput.append("0.")
+        } else if (!isEmpty && isInteger) {
+            tvInput.append(".")
+            isInteger = false
+        }
     }
 }
