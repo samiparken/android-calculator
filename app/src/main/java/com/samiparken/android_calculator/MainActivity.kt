83f8933aae42c0e7eabcaeb8daed88675cdf5bf8
Han-Saem Park <samiparken@gmail.com>
2021-06-21 12:01:28 +0200

    (view as Button).text

--- app/src/main/java/com/samiparken/android_calculator/MainActivity.kt
+++ app/src/main/java/com/samiparken/android_calculator/MainActivity.kt
@@ -3,6 +3,7 @@ package com.samiparken.android_calculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -14,6 +15,6 @@ class MainActivity : AppCompatActivity() {
 
     fun onDigit(view: View) {
         Toast.makeText(this, "Button works", Toast.LENGTH_SHORT).show()
-        tvInput.append("1")
+        tvInput.append( (view as Button).text )
     }
 }
