b0fa7cc41659c13790aac7e57f9596799bbe4656
Han-Saem Park <samiparken@gmail.com>
2021-06-21 09:14:00 +0200

    fix View reference

--- app/src/main/java/com/samiparken/android_calculator/MainActivity.kt
+++ app/src/main/java/com/samiparken/android_calculator/MainActivity.kt
@@ -2,8 +2,9 @@ package com.samiparken.android_calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
+//import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
